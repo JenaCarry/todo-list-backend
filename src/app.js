c7124be8f0ddbc163800c755c4f5f39e4bdc7bae
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-08-21 21:09:22 -0300

    api documentation

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = require("./router");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/swagger", (_req, res) => {
+  return res.sendFile(process.cwd() + "/swagger.json");
+});
+
+app.get("/docs", (req, res) => {
+  return res.sendFile(process.cwd() + "/index.html");
+});
 
 app.use(router);
 
